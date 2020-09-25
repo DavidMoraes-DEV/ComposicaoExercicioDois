0f19b0127fd7ee3da8808a56579c3ee7e4947f6a
David Moraes <investtraderrs@gmail.com>
2020-09-25 13:47:09 -0300

    Composicao Exercicio 2

---
+++ ComposicaoExercicioDois/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ComposicaoExercicioDois
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
